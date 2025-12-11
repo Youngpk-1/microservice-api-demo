7376e7a1e5a82b44ef7d53b0310716fb12ac30ae
isaacwatson30 <isaacwatson30@gmail.com>
2025-12-11 01:15:43 -0600

    install npm for vitest and supertest

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ import express from "express";
 const app = express();
 const PORT = 3001;
 
-app.get("/weather/api", async (req, res) => {
-  res.json({ message: "Welcome to the Weather Microservice!" });
+app.get("/weather/api", (req, res) => {
+  const { zipcode, date } = req.query;
+
+    
+
+
+  res.json({
+    tempHigh: 73,
+    tempLow: 61,
+    conditions: "Partly cloudy",
+    summary: "Cool with light clouds",
+  });
 });
 
 app.listen(PORT, () => {
